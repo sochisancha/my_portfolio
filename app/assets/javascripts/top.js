780d43e29e1a8a8fb443afe7522e184ec6fbb863
sochisanchan <sochi19990403@gmail.com>
2019-06-19 19:40:31 +0900

    デプロイ関連URL変更

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -3,7 +3,7 @@ $(document).ready( function(){
  $('.name').slideUp(3000);
   });
   var url   = location.href;
-  if(url == "https://so-chi-portfolio.herokuapp.com"){
+  if(url == "https://so-chi-portfolio.herokuapp.com/"){
   setTimeout(function() { 
   window.location = "fixed/index";
 }, 5000);
